321aaf80220ae44300bd438a5d02b7297b5e915d
pengshixing <pengshixing17347856051@outlook.com>
2025-09-20 11:35:29 +0800

    init

---
+++ src/stores/counter.ts
@@ -0,0 +1,15 @@
+// https://pinia.esm.dev/introduction.html
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+
+// You can even use a function (similar to a component setup()) to define a Store for more advanced use cases:
+export const useCounterStore = defineStore('counter', () => {
+  const count = ref(0)
+
+  function increment() {
+    count.value++
+  }
+
+  return {count, increment}
+})
