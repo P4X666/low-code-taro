f3214e25fe353c09ddb679d08cc8c139f2b5410e
pengshixing <2644378911@qq.com>
2025-10-16 15:36:51 +0800

    fix

---
+++ src/constants/common.ts
@@ -0,0 +1,12 @@
+export enum TABBARPATH {
+    /** 首页 */
+    HOME = '/pages/home/index',
+    /** 菜单 */
+    MENU = '/pages/category/index',
+    /** 搜索 */
+    SEARCH = '/pages/search/index',
+    /** 购物车 */
+    CART = '/pages/cart/index',
+    /** 账户页 */
+    PROFILE = '/pages/profile/index'
+}
